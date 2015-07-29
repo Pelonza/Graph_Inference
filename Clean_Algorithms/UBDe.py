23b13ae31c75bc4c98b7b3bc3fcbeae52e75cfbc
kschmit1 <kschmit1@GEM-0219-023481.valpo.edu>
2015-07-29 12:34:17 -0500

    Updated Readme & Licensing to LGPL 3.0

--- Clean_Algorithms/UBDe.py
+++ Clean_Algorithms/UBDe.py
@@ -5,15 +5,20 @@ Commit Date 7/27/2015
 You may contact the primary author at:
 karl <dot> schmitt <at> valpo <dot> edu
 
-Licensed under:
+Copyright (C) 2015, Karl R. B. Schmitt & Ralucca Gera
 
-This work is licensed under a Creative Commons 
-Attribution-NonCommercial-NoDerivatives 4.0 International License
-Details of this license can be found at:
-http://creativecommons.org/licenses/by-nc-nd/4.0/
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-Essentially, except by explicit permission, this code may not be adapted,
-shared or sold for any reason.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
 #Ideal Placement
--- Readme.txt
+++ Readme.txt
@@ -14,6 +14,21 @@ It will need to be heavily modified before being used (currently set for 8 algor
 FDD.py and barabasi.gexf are included in main folder to allow test run of 
 graphinfer.py
 
+Copyright (C) 2015, Karl R. B. Schmitt & Ralucca Gera
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 
 Repository and Clean files originally created by:
 Karl Schmitt, Valparaiso University
